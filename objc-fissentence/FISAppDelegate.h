df7f1a71fc20dff5b6105d9d9bbdb50a67c945ab
markedwardmurray <markedwardmurray@gmail.com>
2015-08-27 15:39:26 -0400

    Initial Commit

---
+++ objc-fissentence/FISAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FISAppDelegate.h
+//  objc-fissentence
+//
+//  Created by iOS Staff on 8/27/15
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ objc-fissentence/Resources/Other-Sources/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objc-fissentence
+//
+//  Created by iOS Staff on 8/27/15
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FISAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FISAppDelegate class]));
+    }
+}
