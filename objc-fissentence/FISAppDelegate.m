51d121f35a4c2b5e27db0108d60135bc1941ccbd
markedwardmurray <markedwardmurray@gmail.com>
2015-10-02 09:28:20 -0400

    FISAppDelegate: remove Hello World

--- objc-fissentence/FISAppDelegate.m
+++ objc-fissentence/FISAppDelegate.m
@@ -12,8 +12,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    NSLog(@"Hello, world!");
-    
     return YES;
 }
 
